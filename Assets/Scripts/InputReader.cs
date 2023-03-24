6cd535e0c2e93ed835fe7d845350739d255c9bac
IPashkaYouNot <ppavlichenko@ukr.net>
2023-03-24 20:11:47 +0200

    Added movement to character

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,25 @@
+﻿using Player;
+using UnityEngine;
+
+internal class InputReader : MonoBehaviour
+{
+    [SerializeField] private PlayerEntity _playerEntity;
+
+    private float _horizontalDirection;
+    private float _verticalDirection;
+
+    private void Update()
+    {
+        _horizontalDirection = Input.GetAxisRaw("Horizontal");
+        _verticalDirection = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+            _playerEntity.Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        _playerEntity.MoveHorizontally(_horizontalDirection);
+        _playerEntity.MoveVertically(_verticalDirection);
+    }
+}
